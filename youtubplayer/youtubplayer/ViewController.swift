afc2302aafc19446cd82d4880ea1906afe31db61
MaxCheng <nokia6102@gmail.com>
2017-07-26 15:21:49 +0800

    test1

---
+++ youtubplayer/youtubplayer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  youtubplayer
+//
+//  Created by MaxCheng on 2017/7/26.
+//  Copyright © 2017年 com.USSwiftCoda. All rights reserved.
+//
+
+import UIKit
+import YouTubePlayer
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
